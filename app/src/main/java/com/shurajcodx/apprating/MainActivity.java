bff3c0da594e5ec59b32a209fe2b3d4b90344388
Shuraj Shampang <shuraj.shampang@gmail.com>
2021-03-11 11:41:35 +0545

    fix: add custom textsize option

--- app/src/main/java/com/shurajcodx/apprating/MainActivity.java
+++ app/src/main/java/com/shurajcodx/apprating/MainActivity.java
@@ -1,6 +1,9 @@
 package com.shurajcodx.apprating;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
@@ -13,6 +16,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         loadRatingDialog();
+
+        Button button = findViewById(R.id.rate_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rateDialog();
+            }
+        });
     }
 
     private void loadRatingDialog() {
@@ -26,6 +37,25 @@ public class MainActivity extends AppCompatActivity {
                 .setMessageText(R.string.rating_dialog_custom_message)
                 .setMessageTextColor(R.color.white)
                 .setRateButtonBackground(R.color.colorPrimaryDark)
+                .build();
+
+        appRatingDialog.show();
+    }
+
+    private void rateDialog() {
+        final AppRatingDialog appRatingDialog = new AppRatingDialog.Builder(this)
+                .setTriggerCount(1)
+                .setRepeatCount(1)
+                .setLayoutBackgroundColor(R.color.colorDialogBox)
+                .setIconDrawable(true, ContextCompat.getDrawable(this, R.drawable.love))
+                .setTitleText("Do you like LoveTester?")
+                .setTitleTextColor(R.color.white)
+                .setMessageText("Please take a moment to rate it and help support to improve more new feature.")
+                .setMessageTextSize(R.dimen.text20sp)
+                .setMessageTextColor(R.color.white)
+                .setRateLaterButtonText("Remind me later", null)
+                .setNeverRateButtonText("No, Thanks", null)
+                .setRateButtonBackground(R.color.shurajcodx_skyblue)
                 .build();
 
         appRatingDialog.show();
